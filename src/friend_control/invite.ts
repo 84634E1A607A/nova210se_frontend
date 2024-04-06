ea737244b66d5ca23b3e2821172230a7d2db8a89
sswybd <sswybd@126.com>
2024-04-06 19:26:43 +0800

    fix: (#23) If a pending invitation is found from the receiver to the sender, now it will deal with it correctly.

--- src/friend_control/invite.ts
+++ src/friend_control/invite.ts
@@ -1,9 +1,11 @@
-import { InvitationSourceType } from '../utils/types';
+import { Friend, InvitationSourceType } from '../utils/types';
 
 type InvitationParam = { id: number; source: InvitationSourceType; comment?: string };
-// backend requires comment can't be undefined or null
+// backend requires that comment not be undefined or null
 
-export async function invite(invitationInfo: InvitationParam) {
+export async function invite(
+  invitationInfo: InvitationParam,
+): Promise<{ sendInvitationSuccessful: boolean; friend: Friend | undefined }> {
   if (invitationInfo.comment === undefined) invitationInfo.comment = '';
   try {
     const response = await fetch(process.env.REACT_APP_API_URL!.concat('/friend/invite'), {
@@ -14,10 +16,13 @@ export async function invite(invitationInfo: InvitationParam) {
       },
       credentials: 'include',
     });
-    if (response.ok) return true;
-    return false;
+    if (!response.ok) throw new Error('Failed to send invitation');
+    const data = await response.json(); // data is like: {ok: true, data: null}
+    const friend = data.data;
+    if (friend === null) return { sendInvitationSuccessful: true, friend: undefined };
+    return { sendInvitationSuccessful: true, friend };
   } catch (e) {
     console.error(e);
-    return false;
+    return { sendInvitationSuccessful: false, friend: undefined };
   }
 }
