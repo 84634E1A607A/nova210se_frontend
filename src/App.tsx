96590ee7f4834632f1a866a52fda07cf35a6260c
sswybd <sswybd@126.com>
2024-04-18 23:36:02 +0800

    Feat: Allow for websocket connection.

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,18 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 import { RouterGuard } from './user_control/RouterGuard';
 import { PrimeReactProvider } from 'primereact/api';
+import useWebSocket from 'react-use-websocket';
 
 function App() {
+  const { lastJsonMessage, lastMessage } = useWebSocket(process.env.REACT_APP_WEBSOCKET_URL!, {
+    onOpen: () => {
+      console.log('WebSocket connection established.');
+    },
+    share: true,
+  });
+  console.log('lastJsonMessage', lastJsonMessage);
+  console.log('lastMessage', lastMessage);
+
   return (
     <PrimeReactProvider value={{ unstyled: false }}>
       <div className="App">
